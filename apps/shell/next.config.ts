4d193380b1a2c28a01ae6efb93b5ee1f4397651f
Yacine Kharoubi <yacine.kharoubi@alwasaet.com>
2026-05-02 17:34:01 +0200

    chore: dockerize app

--- apps/shell/next.config.ts
+++ apps/shell/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   reactCompiler: true,
   transpilePackages: ['@repo/ui'],
   async rewrites() {
